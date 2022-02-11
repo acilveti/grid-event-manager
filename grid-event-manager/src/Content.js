81ed6a3a4351e15f30d8083b0cf31bb9e81f4625
acilveti <ander.cilveti.alvarez@gmail.com>
2022-02-11 16:20:04 +0100

    Added basic structure to react header-content-footer

---
+++ grid-event-manager/src/Content.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Content extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Content
+      </div>
+    );
+  }
+}
+
+export default Content;
